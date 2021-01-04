552482d2d44a92093e94a1610742765b0a2702c7
KoutaMatsushita <kouta19960921+github@gmail.com>
2021-01-05 02:24:33 +0900

    add ftc.rb

---
+++ ftc.rb
@@ -0,0 +1,9 @@
+class Ftc < Formula
+  desc "firebase token creator for cli"
+  url "https://github.com/KoutaMatsushita/ftc/releases/download/0.1/ftc"
+  sha256 "900605ed31e5f48fcaa3b419710f4d13effc07f64c6b49e2a64fc2dadffcfcac"
+
+  def install
+    bin.install "ftc"
+  end
+end
